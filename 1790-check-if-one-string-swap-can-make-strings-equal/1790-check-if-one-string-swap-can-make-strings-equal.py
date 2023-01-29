66316ff6f28cad0cc76b08f367318f538735d696
Heba Ahmed <96366380+Heba106@users.noreply.github.com>
2023-01-29 20:13:52 +0200

    Time: 23 ms (98.88%), Space: 14 MB (14.79%) - LeetHub

---
+++ 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.py
@@ -0,0 +1,21 @@
+class Solution:
+    def areAlmostEqual(self, s1: str, s2: str) -> bool:
+        counter=0
+        index=[]
+        for i, a in enumerate(s1):
+            if counter<=2:
+                if a!= s2[i]:
+                    counter+=1
+                    index.append(i)
+            else:
+                return False
+            
+        if counter==2:
+            s1 = list(s1)
+            temp=s1[index[0]]
+            s1[index[0]]=s1[index[1]]
+            s1[index[1]]=temp
+            s1=''.join(s1)
+            print(s1)
+        return True if s1==s2 else False
+        
