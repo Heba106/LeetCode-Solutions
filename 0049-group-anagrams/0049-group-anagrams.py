e051c4fb7cc67530a3d796af0f737d63d8584ab1
Heba Ahmed <96366380+Heba106@users.noreply.github.com>
2023-04-10 16:56:26 +0200

    Time: 115 ms (30.83%), Space: 17.8 MB (53.00%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,15 +1,14 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        result = []
+
         hash_table = {}
         for el in strs:
             sorted_el = sorted(el)
+            print(sorted_el)
             sorted_el = ''.join(sorted_el)
             if sorted_el in hash_table:
                 hash_table[sorted_el].append(el)
             else:
                 hash_table[sorted_el] = [el]
         
-        for item in hash_table:
-            result.append(hash_table[item])
-        return result
+        return hash_table.values()
