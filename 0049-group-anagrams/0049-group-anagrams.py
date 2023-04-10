36c028b00606d9c39644a36493bcbcea1645bac2
Heba Ahmed <96366380+Heba106@users.noreply.github.com>
2023-04-10 16:59:03 +0200

    Time: 106 ms (50.34%), Space: 17.1 MB (93.87%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -4,12 +4,12 @@ class Solution:
         hash_table = {}
         for el in strs:
             sorted_el = sorted(el)
-
             sorted_el = ''.join(sorted_el)
             if sorted_el in hash_table:
                 hash_table[sorted_el].append(el)
             else:
                 hash_table[sorted_el] = [el]
         
-
-        return hash_table.values()
+        for item in hash_table:
+            result.append(hash_table[item])
+        return result
