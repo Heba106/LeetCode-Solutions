2087d8480429109babd5afafdbf1423fd7d87b91
Heba Ahmed <96366380+Heba106@users.noreply.github.com>
2023-04-15 03:42:39 +0200

    Time: 567 ms (99.87%), Space: 27.4 MB (44.14%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -3,11 +3,14 @@ class Solution:
         i = 0
         j = len(height) - 1
         result = 0
-        
+        max_height = max(height)
         while i < j:
             result = max(result,min(height[i],height[j])*(j-i))
             if height[j] < height[i]:
                 j -= 1;
             elif height[i] <= height[j]:
                 i += 1
+                
+            if (j-i) * max_height < result:
+                break
         return result
