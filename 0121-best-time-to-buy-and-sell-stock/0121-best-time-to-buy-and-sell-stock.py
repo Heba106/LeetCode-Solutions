6033f3d50bf97a4978d2a95025581a5b105ca1ae
Heba Ahmed <96366380+Heba106@users.noreply.github.com>
2023-04-15 21:39:43 +0200

    Time: 1007 ms (74.39%), Space: 24.9 MB (79.76%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -6,7 +6,8 @@ class Solution:
         for price in prices:
             if price < lowest:
                 lowest = price
-            profit = max(profit, price - lowest)
+            else:
+                profit = max(profit, price - lowest)
         return profit
 
      
